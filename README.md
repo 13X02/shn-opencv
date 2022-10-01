0fe3b6a8874ac9d5279de4b8950dd1f29c44322a
Abhijith H <abhijithh496@gmail.com>
2022-10-01 22:10:21 +0530

    Text box added and Readme added

---
+++ README.md
@@ -0,0 +1,12 @@
+Human Detection Software
+
+
+modules used:
+    opencv
+    mediapipe
+    haarcascade
+
+
+The software can be run on camera input by changing source
+
+video explaining link:-https://drive.google.com/file/d/1GLUWv1JbhztJSpW4prg42VE50NwFAAqy/view?usp=sharing
--- detector.py
+++ detector.py
@@ -3,7 +3,7 @@ import mediapipe as mp
 
 mp_face_detection = mp.solutions.face_detection
 mp_drawing = mp.solutions.drawing_utils
-cap = cv2.VideoCapture('input.mp4')
+cap = cv2.VideoCapture('Timeline 1.mov')
 human_cascade = cv2.CascadeClassifier('haarcascade_fullbody.xml')
 
 with mp_face_detection.FaceDetection(
@@ -11,6 +11,7 @@ with mp_face_detection.FaceDetection(
     while cap.isOpened():
 
         ret, frame = cap.read()
+        height = int(cap.get(4))
 
         gsimg = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 
@@ -25,11 +26,13 @@ with mp_face_detection.FaceDetection(
         results = face_detection.process(frame)
 
         frame.flags.writeable = True
-
+        font = cv2.FONT_HERSHEY_COMPLEX
         frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
         if results.detections:
           for detection in results.detections:
             mp_drawing.draw_detection(frame, detection)
+            frame = cv2.putText(frame,'HUMANS DETECTED INITIATING TERMINATION....',(10,height-10),font,1,(0,255,0),5,cv2.LINE_AA)
+
 
         cv2.imshow('frame',frame)
         if(cv2.waitKey(1) == ord('q')):
